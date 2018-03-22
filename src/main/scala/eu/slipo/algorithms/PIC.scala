7ba66abc5aa13d6013ff2d1d163d300fd308d839
liyakun <liyakun127@gmail.com>
2018-03-22 13:59:27 +0100

    update sansa

--- src/main/scala/eu/slipo/algorithms/PIC.scala
+++ src/main/scala/eu/slipo/algorithms/PIC.scala
@@ -1,7 +1,11 @@
 package eu.slipo.algorithms
 
+import org.apache.spark.rdd._
+import org.apache.spark.sql._
+import org.apache.spark.graphx.Graph
+import org.apache.spark.graphx.Edge
 import org.apache.spark.mllib.clustering.PowerIterationClustering
-import net.sansa_stack.ml.spark.clustering.{ RDFGraphPICClustering => RDFGraphPICClusteringAlg }
+import net.sansa_stack.ml.spark.clustering.RDFGraphPICClustering
 
 class PIC {
   
@@ -16,8 +20,14 @@ class PIC {
   
   /* Power Iteration using implementation from SANSA
    * */
-  def picSANSA() {
-    
+  def picSANSA(pairwisePOISimilarity: RDD[(Long, Long, Double)], numCentroids: Int, numIterations: Int, sparkSession: SparkSession) {
+    val verticeS = pairwisePOISimilarity.map(f => f._1)
+    val verticeD = pairwisePOISimilarity.map(f => f._2)
+    val indexedMap = verticeS.union(verticeD).distinct().zipWithIndex()
+    val vertices = indexedMap.map(f => (f._2, f._1))
+    val edges = pairwisePOISimilarity.map(f => Edge(f._1, f._2, f._3)) // from similarity to int
+    val similarityGraph = Graph(vertices, edges)
+    //val model = new RDFGraphPICClustering(sparkSession, similarityGraph, numCentroids, numIterations)
   }
   
 }
